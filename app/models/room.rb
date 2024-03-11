806b8a722ccf52f6aa234748e7dbac7857eb9436
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-11 09:51:31 +0900

    画面遷移の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,4 @@
 class Room < ApplicationRecord
   has_many :room_users
-  
-  validates :name, presence: true
-
+  has_many :users, through: :room_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,4 @@ class User < ApplicationRecord
      
        has_many :room_users
        has_many :rooms, through: :room_users
-end
+     end
